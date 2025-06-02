9d9ea9d8d4690590f475fa3900b9aa8b0efa9c25
hp <hp@LAPTOP-01T78CNH>
2025-06-02 07:53:52 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number:");
+string input1 = Console.ReadLine();
+Console.WriteLine("Enter the second number:");
+string input2 = Console.ReadLine();
+
+if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+{
+    double sum = number1 + number2;
+    Console.WriteLine($"The sum of {number1} and {number2} is {sum}.");
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter valid numbers.");
+}
